6f5b1e9d6d72dcb6e5cb4cb3ea2e34753926cf00
idohalevi <ido.h@logz.io>
2018-03-14 12:58:13 +0200

    new version - with shipper

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,68 +3,30 @@ import datetime
 import json
 import logging
 import os
-import sys
-import time
-import urllib2
 
-MAX_BULK_SIZE_IN_BYTES = 1 * 1024 * 1024  # 1 MB
+from shipper import LogzioShipper
 
 # Set logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-# print correct response status code and return True if we need to retry
-def shouldRetry(e):
-    if e.code == 400:
-        logger.error(
-            "Got 400 code from Logz.io. This means that some of your logs are too big, or badly formatted. response: {0}".format(
-                e.reason))
-        return False
-    elif e.code == 401:
-        logger.error("You are not authorized with Logz.io! Token OK? dropping logs...")
-        return False
-    else:
-        logger.error("Got {0} while sending logs to Logz.io, response: {1}".format(e.code, e.reason))
-        return True
-
-
-# send in bulk JSONs object to logzio
-def sendToLogzio(jsonStrLogsList, logzioUrl):
-    headers = {"Content-type": "application/json"}
-    maxRetries = 3
-    sleepBetweenRetries = 5
-    for currTry in reversed(xrange(maxRetries)):
-        request = urllib2.Request(logzioUrl, data='\n'.join(jsonStrLogsList), headers=headers)
-        try:
-            response = urllib2.urlopen(request)
-            logger.info("Successfully sent bulk of " + str(len(jsonStrLogsList)) + " logs to Logz.io!")
-            return
-        except IOError as e:
-            if shouldRetry(e):
-                logger.info("Failure is retriable - Trying {} more times".format(currTry))
-                time.sleep(sleepBetweenRetries)
-            else:
-                raise IOError("Failed to send logs")
-
-    raise RuntimeError("Retries attempts exhausted. Failed sending to Logz.io")
-
-
 # Set statistics parameters
-def setMetricStats(metric, requestsMetaData):
+def _set_metric_stats(metric, requests_meta_data):
+    # type: (dict, dict) -> dict
     stats = {
         "Namespace": metric['Namespace'],
-        "StartTime": requestsMetaData['startTime'],
-        "EndTime": requestsMetaData['endTime'],
-        "Period": requestsMetaData['Period'],
+        "StartTime": requests_meta_data['startTime'],
+        "EndTime": requests_meta_data['endTime'],
+        "Period": requests_meta_data['Period'],
         "MetricName": metric['MetricName'],
         "Dimensions": metric['Dimensions']
     }
 
-    if ('Statistics' in requestsMetaData) and (requestsMetaData['Statistics']):
-        stats['Statistics'] = requestsMetaData['Statistics']
-    elif ('ExtendedStatistics' in requestsMetaData) and (requestsMetaData['ExtendedStatistics']):
-        stats['ExtendedStatistics'] = requestsMetaData['ExtendedStatistics']
+    if ('Statistics' in requests_meta_data) and (requests_meta_data['Statistics']):
+        stats['Statistics'] = requests_meta_data['Statistics']
+    elif ('ExtendedStatistics' in requests_meta_data) and (requests_meta_data['ExtendedStatistics']):
+        stats['ExtendedStatistics'] = requests_meta_data['ExtendedStatistics']
     else:
         stats['Statistics'] = ['Average', 'Minimum', 'Maximum', 'SampleCount', 'Sum']
 
@@ -72,111 +34,105 @@ def setMetricStats(metric, requestsMetaData):
 
 
 # Enrich data point
-def enrichDataPoint(dataPoint, metric):
-    Timestamp = dataPoint.pop('Timestamp')
-    timestamp = Timestamp.isoformat()
-    dataPoint.update({"metric": metric['MetricName'], "@timestamp": timestamp, "Namespace": metric["Namespace"]})
+def _enrich_data_point(data_point, metric):
+    # type: (dict, dict) -> None
+    timestamp = data_point.pop('Timestamp')
+    ts = timestamp.isoformat()
+    data_point.update({"metric": metric['MetricName'], "@timestamp": ts, "Namespace": metric["Namespace"]})
     for dim in metric['Dimensions']:
         key = dim['Name']
         value = dim['Value']
-        dataPoint[key] = value
+        data_point[key] = value
 
 
 # Going over all requests configurations to get statistics
-def getMetricStatistics(client, statsRequestConfigurationsList):
-    bulksOfStatisticsList = []
-    statisticsList = []
-    currentSize = 0
-    for metric in statsRequestConfigurationsList:
+def _get_metric_statistics(cloudwatch, stats_request_configurations_list, logzio_url):
+    # type: ('boto3.client("cloudwatch")', list, str) -> None
+    shipper = LogzioShipper(logzio_url)
+    for metric in stats_request_configurations_list:
         try:
-            response = client.get_metric_statistics(**metric)
+            response = cloudwatch.get_metric_statistics(**metric)
             if response["Datapoints"]:
-                for dataPoint in response["Datapoints"]:
-                    enrichDataPoint(dataPoint, metric)
-                    jDataPoint = json.dumps(dataPoint)
-                    statisticsList.append(jDataPoint)
-                    currentSize += sys.getsizeof(jDataPoint)
-                    if currentSize >= MAX_BULK_SIZE_IN_BYTES:
-                        bulksOfStatisticsList.append(statisticsList)
-                        currentSize = 0
-                        statisticsList = []
-
+                for dp in response["Datapoints"]:
+                    _enrich_data_point(dp, metric)
+                    shipper.add(dp)
         except Exception as e:
             logger.error("Exception from getMetricStatistics: {}".format(e))
             raise
 
-    # add last bulk
-    if statisticsList:
-        bulksOfStatisticsList.append(statisticsList)
-
-    return bulksOfStatisticsList
+    shipper.flush()
 
 
 # Calling list_metrics with the correct configuration
-def listMetric(conf, paginator):
-    responseMetricsList = []
+def _list_metric(conf, paginator):
+    # type: (dict, 'botocore.client.CloudWatch.Paginator.ListMetrics') -> list
+    response_metrics_list = []
     for response in paginator.paginate(**conf):
-        responseMetricsList += response['Metrics']
+        response_metrics_list += response['Metrics']
 
-    return responseMetricsList
+    return response_metrics_list
 
 
 # Retrieve list metrics per configuration
-def getListMetrics(cloudwatch, configurationFilePath, eventTime):
-    responseMetricsList = []
-    requestsMetaData = {}
+def _get_list_metrics(cloudwatch, fp, event_time):
+    # type: ('boto3.client("cloudwatch")', str, str) -> (list, dict)
+    response_metrics_list = []
+    requests_meta_data = {}
     paginator = cloudwatch.get_paginator('list_metrics')
-    with open(configurationFilePath, 'r') as f:
-        jFile = json.load(f)
-        for key in jFile:
+    with open(fp, 'r') as f:
+        jfile = json.load(f)
+        for key in jfile:
             if key != 'Configurations':
-                requestsMetaData[key] = jFile[key]
+                requests_meta_data[key] = jfile[key]
 
-        requestsMetaData.update(getTimes(eventTime, jFile['TimeInterval']))
+                requests_meta_data.update(_get_times(event_time, jfile['TimeInterval']))
 
-        for conf in jFile['Configurations']:
-            responseMetricsList += listMetric(conf, paginator)
+        for conf in jfile['Configurations']:
+            response_metrics_list += _list_metric(conf, paginator)
 
-    logger.info("Received {} possible metric combinations".format(len(responseMetricsList)))
-    return responseMetricsList, requestsMetaData
+    logger.info("Received {} possible metric combinations".format(len(response_metrics_list)))
+    return response_metrics_list, requests_meta_data
 
 
 # Create all needed requests for statistics
-def createStatsRequestList(metricsList, requestsMetaData):
-    statsRequestConfigurationsList = []
-    for metric in metricsList:
+def _create_stats_request_list(metrics_list, requests_meta_data):
+    # type: (list, dict) -> list
+    stats_request_configurations_list = []
+    for metric in metrics_list:
         if metric['Dimensions']:
-            stats = setMetricStats(metric, requestsMetaData)
-            statsRequestConfigurationsList.append(stats)
+            stats = _set_metric_stats(metric, requests_meta_data)
+            stats_request_configurations_list.append(stats)
 
-    return statsRequestConfigurationsList
-
-
-def getTimes(eventTime, timeInterval):
-    now = datetime.datetime.strptime(eventTime, '%Y-%m-%dT%H:%M:%SZ')
-    startTime = (now - datetime.timedelta(minutes=timeInterval)).isoformat()
-    endTime = now.isoformat()
-    return {"startTime": startTime, "endTime": endTime}
+    return stats_request_configurations_list
 
 
-def validateConfigurations():
-    if (not "FILEPATH" in os.environ) or (not "TOKEN" in os.environ) or (not "URL" in os.environ):
+def _get_times(event_time, time_interval):
+    # type: (str, int) -> dict
+    now = datetime.datetime.strptime(event_time, '%Y-%m-%dT%H:%M:%SZ')
+    start_time = (now - datetime.timedelta(minutes=time_interval)).isoformat()
+    end_time = now.isoformat()
+    return {"startTime": start_time, "endTime": end_time}
+
+
+def validate_configurations():
+    # type: (None) -> None
+    if ("FILEPATH" not in os.environ) or ("TOKEN" not in os.environ) or ("URL" not in os.environ):
         logger.error("Some environment variables are missing.")
         raise RuntimeError
 
-    configurationFilePath = os.environ["FILEPATH"]
+    configuration_fp = os.environ["FILEPATH"]
     try:
-        with open(configurationFilePath, 'r') as f:
+        with open(configuration_fp, 'r') as f:
             try:
-                jFile = json.load(f)
-                timeInterval = jFile['TimeInterval']
-                period = jFile['Period']
-                configurations = jFile['Configurations']
+                jfile = json.load(f)
+                time_interval = jfile['TimeInterval']
+                period = jfile['Period']
+                configurations = jfile['Configurations']
             except (ValueError, KeyError) as e:
                 logger.error("Error in your configuration file format: {}".format(e))
                 raise
             else:
-                if not isinstance(timeInterval, int):
+                if not isinstance(time_interval, int):
                     logger.error("Error in your configuration file: TimeInterval should be int(min)")
                     raise RuntimeError
 
@@ -184,20 +140,20 @@ def validateConfigurations():
                     logger.error("Error in your configuration file: Period should be int(sec)")
                     raise RuntimeError
 
-                if (timeInterval * 60) < period:
+                if (time_interval * 60) < period:
                     logger.error("Error in your configuration file: TimeInterval can't be < period")
                     raise RuntimeError
 
-                statistics = jFile.get("Statistics", [])
-                optionalStatistics = ["Average", "Minimum", "Maximum", "SampleCount", "Sum"]
+                statistics = jfile.get("Statistics", [])
+                optional_statistics = ["Average", "Minimum", "Maximum", "SampleCount", "Sum"]
                 for s in statistics:
-                    if not s in optionalStatistics:
+                    if s not in optional_statistics:
                         logger.error(
-                            "Error in your configuration file: {0} is not part of {1}".format(s, optionalStatistics))
+                            "Error in your configuration file: {0} is not part of {1}".format(s, optional_statistics))
                         raise RuntimeError
 
-                extendedStatistics = jFile.get("ExtendedStatistics", [])
-                if statistics and extendedStatistics:
+                extended_statistics = jfile.get("ExtendedStatistics", [])
+                if statistics and extended_statistics:
                     logger.error("Can't have both Statistics and ExtendedStatistics (according to boto3 documentation)")
                     raise RuntimeError
 
@@ -208,8 +164,8 @@ def validateConfigurations():
                         raise RuntimeError
 
                     try:
-                        metricName = conf["MetricName"]
-                        if not isinstance(metricName, str):
+                        metric_name = conf["MetricName"]
+                        if not isinstance(metric_name, str):
                             logger.error("Error in your configuration file: Period should be int(sec)")
                             raise RuntimeError
                     except KeyError:
@@ -220,26 +176,22 @@ def validateConfigurations():
         logger.error("You have an error in the configuration file path: {}".format(e))
         raise
 
-    return timeInterval
-
 
 def lambda_handler(event, context):
-    validateConfigurations()
+    # type: (dict, 'LambdaContext') -> None
+    validate_configurations()
     # Use time from event
     try:
-        eventTime = event['time']
+        event_time = event['time']
     except KeyError:
         logger.error("No time field from the event")
         raise
 
-    logger.info("Collecting Lambda metrics at {}".format(eventTime))
-    logzioUrl = "{0}/?token={1}&type=cloudwatchmetrics".format(os.environ['URL'], os.environ['TOKEN'])
-    configurationFilePath = os.environ['FILEPATH']
+    logger.info("Collecting Lambda metrics at {}".format(event_time))
+    logzio_url = "{0}/?token={1}&type=cloudwatchmetrics".format(os.environ['URL'], os.environ['TOKEN'])
+    configuration_fp = os.environ['FILEPATH']
     cloudwatch = boto3.client("cloudwatch")
 
-    metricsList, requestsMetaData = getListMetrics(cloudwatch, configurationFilePath, eventTime)
-    statsRequestConfigurationsList = createStatsRequestList(metricsList, requestsMetaData)
-    metricsJsonList = getMetricStatistics(cloudwatch, statsRequestConfigurationsList)
-
-    for bulkJsonList in metricsJsonList:
-        sendToLogzio(bulkJsonList, logzioUrl)
+    metrics_list, requests_meta_data = _get_list_metrics(cloudwatch, configuration_fp, event_time)
+    stats_request_configurations_list = _create_stats_request_list(metrics_list, requests_meta_data)
+    _get_metric_statistics(cloudwatch, stats_request_configurations_list, logzio_url)
